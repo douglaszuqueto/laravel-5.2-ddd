b577f0fcc0fd2de8be53f3a0b2a6f1cb370af1a7
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-06-07 21:06:14 -0300

    Laravel DDD - Base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - DDD</div>
+                <div class="title">Laravel 5.2 - DDD</div>
             </div>
         </div>
     </body>
